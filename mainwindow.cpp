b3504fe8d64a47602902613237af50b2647449d8
Danilo <danilo.santos.vieira.contato@gmail.com>
2019-07-21 16:44:25 -0300

    Novas Alterações

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -39,6 +39,8 @@ void MainWindow::criarConects()
 	connect(ui->pushButtonMenos, SIGNAL(clicked()), this, SLOT(slotSetValueWindow()));
 	connect(ui->pushButtonVezes, SIGNAL(clicked()), this, SLOT(slotSetValueWindow()));
 	connect(ui->pushButtonDividir, SIGNAL(clicked()), this, SLOT(slotSetValueWindow()));
+	connect(ui->pushButtonIgual, SIGNAL(clicked()), this, SLOT(slotSetValueWindow()));
+	connect(ui->pushButtonPoint, SIGNAL(clicked()), this, SLOT(slotSetValueWindow()));
 
 
 }
@@ -49,46 +51,116 @@ void MainWindow::setGlobalValue(double value, QString op)
 	{
 		this->globalVar = this->adicao(this->globalVar, value);
 	}
-	
+	else if (op.compare("-") == 0) {
+		this->globalVar = this->subtracao(this->globalVar, value);
+	}
+	else if (op.compare("x") == 0) {
+		this->globalVar = this->multiplicacao(this->globalVar, value);
+	}
+	else if (op.compare("÷") == 0) {
+		this->globalVar = this->divisao(this->globalVar, value);
+	}
 }
 
 
 void MainWindow::slotClearWindowValue()
 {
 	this->ui->lineEdit->clear();
+	this->globalVar = 0;
+	this->signal = "";
 }
 
 void MainWindow::slotSetValueWindow()
 {
 	QPushButton* pButton = qobject_cast<QPushButton*>(sender());
+	QString buttonText = pButton->text();
 
 
 	if (pButton->text().compare("+") == 0)
 	{
-		this->signal = "+";
-		this->setGlobalValue(this->ui->lineEdit->text().toDouble(), pButton->text());
-	
+        if(this->signal != "n")
+        {
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().remove(-1,1).append(buttonText));
+            this->signal = this->signal_2 = buttonText;
+        }
+        else
+        {
+            this->setGlobalValue(this->ui->lineEdit->text().toDouble(), this->signal_2);
+            this->signal = this->signal_2 = "+";
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().append(ui->lineEdit->text().append(buttonText)));
+            ui->lineEdit->setText(QString::number(this->globalVar));
+        }
 	}
 	else if(pButton->text().compare("-") == 0)
 	{
-		QMessageBox::warning(this, "", "-");
+        if(this->signal != "n")
+        {
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().remove(-1,1).append(buttonText));
+            this->signal = this->signal_2 = buttonText;
+        }
+        else
+        {
+            this->setGlobalValue(this->ui->lineEdit->text().toDouble(), this->signal_2);
+            this->signal = this->signal_2 = "-";
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().append(ui->lineEdit->text().append(buttonText)));
+            ui->lineEdit->setText(QString::number(this->globalVar));
+        }
 	}
 	else if (pButton->text().compare("x") == 0)
 	{
-		QMessageBox::warning(this, "", "x");
+        if(this->signal != "n")
+        {
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().remove(-1,1).append(buttonText));
+            this->signal = this->signal_2 = buttonText;
+        }
+        else
+        {
+            this->setGlobalValue(this->ui->lineEdit->text().toDouble(), this->signal_2);
+            this->signal = this->signal_2 = "x";
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().append(ui->lineEdit->text().append(buttonText)));
+            ui->lineEdit->setText(QString::number(this->globalVar));
+        }
 	}
 	else if (pButton->text().compare("÷") == 0) //246 ascii
 	{
-		QMessageBox::warning(this, "", "÷");
+        if(this->signal != "n")
+        {
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().remove(-1,1).append(buttonText));
+            this->signal = this->signal_2 = buttonText;
+        }
+        else
+        {
+            this->setGlobalValue(this->ui->lineEdit->text().toDouble(), this->signal_2);
+            this->signal = this->signal_2 = "÷";
+            ui->lineEdit_2->setText(ui->lineEdit_2->text().append(ui->lineEdit->text().append(buttonText)));
+            ui->lineEdit->setText(QString::number(this->globalVar));
+        }
+	}
+	else if (pButton->text().compare("=") == 0) {
+		if (this->signal == "n") {
+            this->setGlobalValue(this->ui->lineEdit->text().toDouble(), this->signal_2);
+            ui->lineEdit->setText(QString::number(this->globalVar));
+            ui->lineEdit_2->clear();
+		}
+		else {
+            //ui->lineEdit->setText(QString::number(this->globalVar));
+            //ui->lineEdit_2->clear();
+            //this->globalVar = 0;
+            //this->signal = "";
+            this->setGlobalValue(this->ui->lineEdit->text().toDouble(),this->signal_2);
+            ui->lineEdit_2->clear();
+            ui->lineEdit->setText(QString::number(this->globalVar));
+		}
 	}
 	else // this is the type we expect
 	{
-		if (ui->lineEdit->text().compare("0") == 0) {
+		if ( (ui->lineEdit->text().compare("0") == 0) || (this->signal != "n") ) {
 			ui->lineEdit->clear();
 		}
 
-		QString buttonText = pButton->text();
+		//QString buttonText = pButton->text();
 		ui->lineEdit->setText(ui->lineEdit->text().append(buttonText));
+		this->signal = "n";
 	}
 
 	
@@ -99,6 +171,7 @@ void MainWindow::slotSetLineEdit(double _globalVar)
 	this->ui->lineEdit->clear();
 	this->ui->lineEdit->setText(QString::number(_globalVar));
 }
+
 
 
 void MainWindow::slotClearWindow() {
@@ -113,33 +186,37 @@ void MainWindow::slotClearWindow() {
 
 	//limpar a varivel global
 	this->globalVar = 0;
+
+    //limpar a variavel global dos sinais
+    this->signal = "";
+    this->signal_2 = "+";
 }
 
 double MainWindow::adicao(double A, double B) {
 	return A + B;
 }
 
-//void MainWindow::slotOperacaoWindow() {
-//	
-//}
-
 //int subtracao(int a, int b);
-int MainWindow::subtracao(int A, int B) {
-	int C = A - B;
+double MainWindow::subtracao(double A, double B) {
+    double C = A - B;
 	return C;
 }
 
 //int divisao(int a, int b);
-int MainWindow::divisao(int A, int B) {
+double MainWindow::divisao(double A, double B) {
 	if (B == 0) {
-
+		QMessageBox::warning(this, "", "Divisao por zero");
 	}
-	int C = A / B;
+    double C = A / B;
 	return C;
 }
 
 //int multiplicacao(int a, int b);
-int MainWindow::multiplicacao(int A, int B) {
-	int C = A * B;
+double MainWindow::multiplicacao(double A, double B) {
+    double C = A * B;
 	return C;
 }
+
+//void MainWindow::slotOperacaoWindow() {
+//	
+//}
--- mainwindow.h
+++ mainwindow.h
@@ -23,9 +23,11 @@ private:
 	Ui::MainWindow *ui;
 
 	//varivel Global de REsultado
-	int globalVar = 0;
+	double globalVar = 0;
 	//var sinal atual
 	QString signal =  "";
+    //variavel para segundo sinal
+    QString signal_2 = "+";
 
 
     //Cria os connects da classe MainWindow
@@ -39,13 +41,13 @@ private:
 	double adicao(double A, double B);
 
     //int subtracao(int a, int b);
-	int subtracao(int A, int B);
+    double subtracao(double A, double B);
 
     //int divisao(int a, int b);
-	int divisao(int A, int B);
+    double divisao(double A, double B);
 
     //int multiplicacao(int a, int b);
-	int multiplicacao(int A, int B);
+    double multiplicacao(double A, double B);
 	
 public slots:
 
